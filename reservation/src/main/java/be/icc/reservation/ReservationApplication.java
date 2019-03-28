365e049a58ac03ab1c500f7fd102ef62353244e0
dorian scohier <dorian.scohier@minfin.fed.be>
2019-03-28 20:47:10 +0100

    Config jsp

--- reservation/src/main/java/be/icc/reservation/ReservationApplication.java
+++ reservation/src/main/java/be/icc/reservation/ReservationApplication.java
@@ -2,9 +2,10 @@ package be.icc.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 //TODO (exclude = SecurityAutoConfiguration.class) A ajouter pour la desactivation de la sécurité
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ReservationApplication {
 
 	public static void main(String[] args) {
