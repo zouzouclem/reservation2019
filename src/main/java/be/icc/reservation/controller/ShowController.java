0e52e564ecadb708570946312d2f398f7804432d
Leila <ltanouti@gmail.com>
2019-05-02 19:48:40 +0200

    merge - shows

--- src/main/java/be/icc/reservation/controller/ShowController.java
+++ src/main/java/be/icc/reservation/controller/ShowController.java
@@ -4,16 +4,11 @@ package be.icc.reservation.controller;
 import be.icc.reservation.entity.Shows;
 import be.icc.reservation.service.ShowService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Autowired;
-import be.icc.reservation.service.ShowService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 
 @Controller
