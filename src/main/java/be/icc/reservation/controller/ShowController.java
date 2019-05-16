5e7acf9dfa498660c6f3efb1988cf2b1d20c919d
Philippe <philkoumi17@gmail.com>
2019-05-16 20:05:14 +0200

    Mise à jour showDB fonctionnel

--- src/main/java/be/icc/reservation/controller/ShowController.java
+++ src/main/java/be/icc/reservation/controller/ShowController.java
@@ -1,22 +1,27 @@
 package be.icc.reservation.controller;
 
-
 import be.icc.reservation.entity.Locations;
 import be.icc.reservation.entity.Shows;
+import be.icc.reservation.entity.Users;
 import be.icc.reservation.form.ShowForm;
+import be.icc.reservation.form.SignupForm;
 import be.icc.reservation.service.LocationsService;
+import be.icc.reservation.entity.Shows;
 import be.icc.reservation.service.ShowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.math.BigDecimal;
+
 import java.util.List;
 
 
@@ -42,14 +47,14 @@ public class ShowController {
         return "show/addShow";
     }
 
-    @RequestMapping(value = "show/show/add", method = RequestMethod.POST)
+    @RequestMapping(value = "/show/add", method = RequestMethod.POST)
     public String addShowDB(@ModelAttribute("showForm") @Valid ShowForm showForm, BindingResult result,
-                            RedirectAttributes attr, Model model) {
-        /*if (result.hasErrors()) {
+                         RedirectAttributes attr, Model model) {
+        if (result.hasErrors()) {
             attr.addFlashAttribute("org.springframework.validation.BindingResult.showForm", result);
             attr.addFlashAttribute("showForm", showForm);
-            return "redirect:/show/addShow";
-        }*/
+            return "show/addShow";
+        }
 
         Shows s = new Shows();
         s.setSlug(showForm.getSlug());
@@ -57,15 +62,49 @@ public class ShowController {
         s.setLocationId(loc);
         s.setBookable(showForm.isBookable());
         s.setPosterUrl(showForm.getPosterURL());
-        s.setPrice(BigDecimal.valueOf(showForm.getPrice()));
+        s.setPrice(showForm.getPrice());
         s.setTitle(showForm.getTitle());
         showService.saveShow(s);
         return "redirect:/show";
     }
 
-    @RequestMapping(value = "/show/update")
-    public String updateSpectacle() {
+    @RequestMapping(value = "/show/update/{id}")
+    public String updateSpectacle(Model model, @PathVariable int id)
+    {
+        Shows sho = showService.findById(id);
+        ShowForm showForm = new ShowForm();
+        showForm.setId(sho.getId());
+        showForm.setSlug(sho.getSlug());
+        showForm.setTitle(sho.getTitle());
+        showForm.setPosterURL(sho.getPosterUrl());
+        showForm.setLocation(sho.getLocationId().getId());
+        showForm.setBookable(sho.isBookable());
+        showForm.setPrice((BigDecimal)sho.getPrice());
+        model.addAttribute("showForm", showForm);
+        model.addAttribute("locationsList", locationsService.findAllLocations());
         return "show/updateShow";
     }
 
+    @RequestMapping(value = "/show/update", method = RequestMethod.POST)
+    public String updateSpectacleDB(@ModelAttribute("showForm") @Valid ShowForm showForm, BindingResult result,
+                                  RedirectAttributes attr)
+    {
+        if(result.hasErrors())
+        {
+            attr.addFlashAttribute("org.springframework.validation.BindingResult.showForm", result);
+            attr.addFlashAttribute("showForm", showForm);
+            return "redirect:/show/updateShow";
+        }
+
+        Shows s = showService.findById(showForm.getId());
+        s.setSlug(showForm.getSlug());
+        Locations l = locationsService.findLocationsById(showForm.getLocation());
+        s.setLocationId(l);
+        s.setBookable(showForm.isBookable());
+        s.setPosterUrl(showForm.getPosterURL());
+        s.setPrice(showForm.getPrice());
+        s.setTitle(showForm.getTitle());
+        showService.updateShow(s);
+        return "show/updateShow";
+    }
 }
--- src/main/webapp/WEB-INF/jsp/show/updateShow.jsp
+++ src/main/webapp/WEB-INF/jsp/show/updateShow.jsp
@@ -2,6 +2,7 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <html>
 
 <jsp:include page="../assets/header.jsp"/>
@@ -9,7 +10,69 @@
 <body>
 <jsp:include page="../assets/menu.jsp"/>
 
-Modifier un spectacle
+    <c:if test="${showForm.id != null}">
+        <div>
+
+            <form:form method="post" action="/show/update" modelAttribute="showForm">
+
+                <div style="visibility: hidden;">
+                    <form:input type="text" path="id" cssClass="form-control" id="title"/>
+                </div>
+                <fieldset>
+                    <c:if test="${not empty error}">
+                        <label class="error">${error}/></label>
+                    </c:if>
+                    <h2>Shows</h2>
+                    <div>
+                        <label for="slug">Slug</label>
+                        <div>
+                            <form:input type="text" path="slug" id="slug"/>
+                            <form:errors path="slug" cssClass="error"/>
+                        </div>
+                    </div>
+                    <div>
+                        <label for="title">Title</label>
+                        <div>
+                            <form:input type="text" path="title" id="title"/>
+                            <form:errors path="title" cssClass="error"/>
+                        </div>
+                    </div>
+                    <div>
+                        <label for="posterURL">Poster_URL</label>
+                        <div>
+                            <form:input type="text" path="posterURL" id="posterURL"/>
+                            <form:errors path="posterURL" cssClass="error"/>
+                        </div>
+                    </div>
+                    <div>
+                        <label for="location">Location</label>
+                        <div>
+                            <form:select path="location" items="${locationsList}"/>
+                            <form:errors path="location" cssClass="error"/>
+                        </div>
+                    </div>
+                    <div>
+                        <label for="bookable">Bookable</label>
+                        <div>
+                            <form:checkbox path="bookable" id="bookable"/>
+                            <form:errors path="bookable" cssClass="error"/>
+                        </div>
+                    </div>
+                    <div>
+                        <label for="price">Price</label>
+                        <div>
+                            <form:input type="text" path="price" id="price"/>
+                            <form:errors path="price" cssClass="error"/>
+                        </div>
+                    </div>
+                    <br />
+                    <div>
+                        <input type="submit" value="Mettre à jour">
+                    </div>
+                </fieldset>
+            </form:form>
+        </div>
+    </c:if>
 
 <jsp:include page="../assets/footer.jsp"/>
 
