149181121ca764f1f903f86196fcd57b95abe40d
clem <clementinevallet@hotmail.fr>
2019-06-10 13:22:37 +0200

    admin test

--- src/main/java/be/icc/reservation/controller/HomeController.java
+++ src/main/java/be/icc/reservation/controller/HomeController.java
@@ -1,10 +1,13 @@
 package be.icc.reservation.controller;
 
 
+import be.icc.reservation.entity.Roles;
+import be.icc.reservation.entity.Users;
 import be.icc.reservation.service.RolesService;
 import be.icc.reservation.service.ShowService;
 import be.icc.reservation.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +32,7 @@ public class HomeController {
 
     @RequestMapping(value = "/")
     public String home(HttpServletRequest request) {
+        initData();
         if ((String) request.getSession().getAttribute("styleColor") == null) {
             request.getSession().setAttribute("styleColor", "defaut");
         }
@@ -38,6 +42,26 @@ public class HomeController {
     @RequestMapping(value = "/accessDenied")
     public String accessDenied() {
         return "accessDenied";
+    }
+
+    private void initData(){
+        //create admin
+
+        Users admin= userService.findByLoginAndPassword("admin", "admin");
+        if(admin== null) {
+            admin= new Users();
+            admin.setLogin("admin");
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            String hashedPassword = encoder.encode("admin");
+            admin.setPassword(hashedPassword);
+            Roles role = rolesService.createOrgetIfExists("ROLE_ADMIN");
+            admin.setRole(role);
+            admin.setEmail("admin@admin.admin");
+            admin.setFirstname("admin");
+            admin.setLastname("admin");
+            admin.setLangue("en");
+            userService.save(admin);
+        }
     }
 
 }
